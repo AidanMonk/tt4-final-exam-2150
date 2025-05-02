107226c48916169cdfb5ca4fc76c5ba461607af0
AidanMonk <AidanMonk@outlook.com>
2025-05-02 13:05:47 -0400

    added basic recipe api

---
+++ backend/RecipeApi/Controllers/RecipeController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeApi.Data;
+using RecipeApi.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly RecipeDbContext _context;
+
+        public RecipeController(RecipeDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Recipe
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Recipe>>> GetRecipes()
+        {
+            return Ok(await _context.Recipes.ToListAsync());
+        }
+
+        // GET: api/Recipe/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Recipe>> GetRecipe(int id)
+        {
+            var recipe = await _context.Recipes.FindAsync(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+            return Ok(recipe);
+        }
+
+        // POST: api/Recipe
+        [HttpPost]
+        public async Task<ActionResult<Recipe>> PostRecipe([FromBody] Recipe recipe)
+        {
+            _context.Recipes.Add(recipe);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetRecipe), new { id = recipe.Id }, recipe);
+        }
+    }
+}
---
+++ backend/RecipeApi/Models/Recipe.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecipeApi.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Ingredients { get; set; } // Storing ingredients as a string (you could also normalize this)
+        public string Instructions { get; set; }
+        public TimeSpan PrepTime { get; set; }
+    }
+}
